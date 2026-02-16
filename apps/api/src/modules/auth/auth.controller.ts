aa3d1f9565a9ea768dc4251a56b712887eccb313
Satyam's Macbook <sunny@Satyams-Macbook.lan>
2026-02-16 13:30:07 +0530

    feat: feat: add the jwt strategy and implement the admin routing

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserRole } from 'src/database/postgres/entities/user.entity';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { RolesGuard } from './guards/roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -27,5 +31,18 @@ export class AuthController {
   @Post('login')
   login(@Body() body: { email: string; password: string }) {
     return this.authService.login(body.email, body.password);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@GetUser() user: any) {
+    return user;
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
+  @Get('admin-only')
+  adminRoute() {
+    return { message: 'Welcome Admin 👑' };
   }
 }
--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller'; 
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -17,8 +22,8 @@ import { AuthController } from './auth.controller';
       }),
     }),
   ],
-controllers: [AuthController],
-  providers: [AuthService],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard, RolesGuard],
   exports: [AuthService],
 })
 export class AuthModule {}
